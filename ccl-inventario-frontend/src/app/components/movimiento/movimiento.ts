ac5f4737c3833d55708b91dd8edd65665a8ed9f8
Daniel Contreras <danielorlando520@gmail.com>
2026-05-25 08:12:20 -0500

    Commit 01
    -Se recarga la carpeta del front: ccl-inventario-frontend al repo.

---
+++ ccl-inventario-frontend/src/app/components/movimiento/movimiento.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimiento',
+  imports: [],
+  templateUrl: './movimiento.html',
+  styleUrl: './movimiento.css',
+})
+export class Movimiento {}
