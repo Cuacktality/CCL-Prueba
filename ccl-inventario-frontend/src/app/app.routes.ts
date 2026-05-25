194ff27b4d30dd7546003aed7cc9fb0acd8784e7
Daniel Contreras <danielorlando520@gmail.com>
2026-05-25 10:42:21 -0500

    Commit 07 [Angular]
    -Se realiza la configuración de la app en Angular, (Interceptores, Rutas, HTML y CSS)

--- ccl-inventario-frontend/src/app/app.routes.ts
+++ ccl-inventario-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
+import { InventarioComponent } from './components/inventario/inventario';
+import { MovimientoComponent } from './components/movimiento/movimiento';
+import { authGuard } from './guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'inventario', component: InventarioComponent, canActivate: [authGuard] }
+];
--- ccl-inventario-frontend/src/app/services/auth.ts
+++ ccl-inventario-frontend/src/app/services/auth.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Auth {}
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private apiUrl = 'http://localhost:5144/auth';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(username: string, password: string) {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, { username, password });
+  }
+
+  guardarToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  cerrarSesion() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  estaAutenticado(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
