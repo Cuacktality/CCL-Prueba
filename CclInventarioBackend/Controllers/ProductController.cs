746b170041a4820b3ced9376c4c85165793793df
Daniel Contreras <danielorlando520@gmail.com>
2026-05-25 12:20:14 -0500

    Update ProductController.cs
    -Verificación de seguridad del tipo en el request.
    Se añade un trim para eliminar los espacios y se añade un ToLower para que la cadena sea siempre en minúsculas y no haya error de comparación.
    Se cambia el operador == por string.Equals.

--- CclInventarioBackend/Controllers/ProductController.cs
+++ CclInventarioBackend/Controllers/ProductController.cs
@@ -26,12 +26,14 @@ public class ProductController : ControllerBase {
     public async Task<IActionResult> RegistrarMovimiento([FromBody] MovimientoRequest request) {
         var producto = await _db.Productos.FindAsync(request.ProductoId);
 
+        var tipo = request.Tipo.Trim().ToLower();
+
         if (producto == null) return NotFound(new { message = "Producto no encontrado" });
-        if (request.Tipo == "salida" && producto.Cantidad < request.Cantidad) 
+        if (tipo.Equals("salida") && producto.Cantidad < request.Cantidad) 
             return BadRequest(new { message = "Stock Insuficiente. No se puede realizar la operación" });
 
-        if (request.Tipo == "entrada") producto.Cantidad += request.Cantidad;
-        else if (request.Tipo == "salida") producto.Cantidad -= request.Cantidad;
+        if (tipo.Equals("entrada")) producto.Cantidad += request.Cantidad;
+        else if (tipo.Equals("salida")) producto.Cantidad -= request.Cantidad;
         else return BadRequest(new { message = "Tipo debe ser 'ENTRADA' o 'SALIDA'" });
 
         await _db.SaveChangesAsync();
