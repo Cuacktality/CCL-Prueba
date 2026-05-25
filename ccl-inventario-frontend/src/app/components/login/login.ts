a9e31d2ab27fd8cb92ed4f22720f6ab513c43f96
Daniel Contreras <danielorlando520@gmail.com>
2026-05-25 10:59:59 -0500

    Commit 08 [Login]
    -Se realiza el frente para el commit.
    -Se corrige la clave para generar el Token en el appsettings ya que se presentó un error por la cadena Key tan corta.

--- ccl-inventario-frontend/src/app/components/login/login.ts
+++ ccl-inventario-frontend/src/app/components/login/login.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
+  cargando = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    if (!this.username || !this.password) {
+      this.error = 'Usuario y contraseña son obligatorios';
+      return;
+    }
+
+    this.cargando = true;
+    this.error = '';
+
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res) => {
+        this.authService.guardarToken(res.token);
+        this.router.navigate(['/inventario']);
+      },
+      error: () => {
+        this.error = 'Credenciales incorrectas';
+        this.cargando = false;
+      }
+    });
+  }
+}
